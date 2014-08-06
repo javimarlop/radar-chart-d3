ba8928e373dbcbd96e0a94a8a7bdb51aa76c0dbb
Javier <javier.martinez@um.es>
2014-08-06 07:54:42 +0200

    looping through wdpa ids for exporting segments into one json

--- script.R
+++ script.R
@@ -1,24 +1,37 @@
+
+hri<-read.table('hri_results.csv',sep=' ',header=T,dec='.')
+# merge with segmentation output shapefile table
+# dbfdump
+
+
+export_max_json<-function(){
 library(RJSONIO)
 library(reshape)
 
-hri<-read.table('hri_results.csv',sep=' ',header=T,dec='.')
-hri2<-hri[,c(2,seq(13,29,2))]
-hri3<-melt(hri2[1:3,],'wdpaid')
+pas<-unique(hri$wdpa_id)
 
+for (j in 1:length(pas)){
+index<-hri$wdpa_id==pas[j]
+
+hri2<-hri[j,c(2,seq(13,29,2))]
+hri3<-melt(hri2,'wdpaid')
 names(hri3)<-c('className','axis','value')
+hri3l<-hri3
+hri3l$value<-log(hri3$value+1))
 
-by(hri3[-1],hri3$className,function(x )toJSONarray(x))->data
+by(hri3l[-1],hri3l$className,function(x )toJSONarray(x))->data
 
-export_max_json<-function(){
-for (i in 1:length(data)){
 sink(paste('data_max_',names(data[i]),sep=''))
 cat("var data = [",fill=TRUE)
+for (i in 1:length(data)){
 cat("{",fill=TRUE)
 cat(paste("className: '",names(data[i]),"',",sep=''),fill=TRUE)
 cat("axes: [,",fill=TRUE)
 cat(data[i])
 sink()
 }
+}
+
 }
 
 
