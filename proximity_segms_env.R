196d343558a876a93d06b35fd7fd2d07b17fbb62
Javier MARTINEZ LOPEZ <javier.martinez@um.es>
2014-08-26 15:57:24 +0200

    last

---
+++ proximity_segms_env.R
@@ -0,0 +1,13 @@
+# Based on variables average for each segment
+
+hri<-read.table('hri_results.csv',sep=' ',header=T,dec='.')
+hri[c(1:2,30:38)]->means
+randomForest(means[,3:11])->rfmeans
+MDSplot(rfmeans,factor(means$wdpaid)) # add site wdpaid and use it as colour
+plot(rfmeans$proximity)
+as.data.frame(rfmeans$proximity)->prox_env
+as.data.frame(apply(prox_env,2,mean))->prox_means
+plot(prox_means[,1])
+
+# we can then use mantel test with distance matrices based on averages of env vars or with proximities to assess overall fit (from vegan R library)
+mantel(prox_env,prox_bio)
